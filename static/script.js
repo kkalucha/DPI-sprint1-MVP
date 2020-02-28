89bb7e19919dc6a551353f67ab7c237dc823867f
Ivan Barral <51125955+ibarral18@users.noreply.github.com>
2020-02-28 13:50:05 -0500

    functionality added for NavBar

--- static/script.js
+++ static/script.js
@@ -1,3 +1,13 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "200px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 var x, i, j, selElmnt, a, b, c;
 /* Look for any elements with the class "custom-select": */
 x = document.getElementsByClassName("custom-select");
